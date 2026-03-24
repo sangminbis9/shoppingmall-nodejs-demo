cc3c4bc3560d78b6f237a8ebb4fa399dbe80bff0
sangmin <sangminbis9@gmail.com>
2026-03-24 19:08:19 +0900

    mysql2

--- productDisplay.js
+++ productDisplay.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const path = require('path');
 const fs = require('fs');
 const dotenv = require('dotenv');
